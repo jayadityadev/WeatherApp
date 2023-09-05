e35441024f126665878408ddbb2507c613d0ee69
Jayaditya Dev <jayadityadev26@outlook.com>
2023-09-05 22:45:59 +0530

    Added exception handler for invalid city.

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from functions import weather
 
 
 if __name__ == '__main__':
+    try:
+        print("\nWelcome to Weather App. Created by Jayaditya Dev in Python.")
+        city = input("Enter the name of the City: ")
 
-    print("\nWelcome to Weather App. Created by Jayaditya Dev in Python.")
-    city = input("Enter the name of the City: ")
+        print(f"\nPlace: {weather(city)['place']}")
+        print(f"Weather: {weather(city)['weather']}")
+        print(f"Temperature: {weather(city)['temp_c']}°C / {weather(city)['temp_f']}°F")
+        print(f"Local Time: {weather(city)['local_time']}")
+        print(f"Day/Night: {weather(city)['day_night']}")
 
-    print(f"\nPlace: {weather(city)['place']}")
-    print(f"Weather: {weather(city)['weather']}")
-    print(f"Temperature: {weather(city)['temp_c']}°C / {weather(city)['temp_f']}°F")
-    print(f"Local Time: {weather(city)['local_time']}")
-    print(f"Day/Night: {weather(city)['day_night']}")
-
-    print("\nThank You!")
+        print("\nThank You!")
+    except KeyError:
+        print("\nPlease enter a valid city name.")
