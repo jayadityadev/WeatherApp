82012d9e3e0d69d77e3739b72c41c916aa3f0189
Jayaditya Dev <jayadityadev26@outlook.com>
2023-09-07 00:34:28 +0530

    Added 'last_updated' in functions.weather and optimized main.py

--- functions.py
+++ functions.py
@@ -9,13 +9,16 @@ def request_handler(city):
 
 def weather(city):
     place = request_handler(city)["location"]["name"]
+    region = request_handler(city)["location"]["region"]
+    country = request_handler(city)["location"]["country"]
     condition = request_handler(city)["current"]["condition"]["text"]
     temp_c = request_handler(city)["current"]["temp_c"]
     temp_f = request_handler(city)["current"]["temp_f"]
     local_time = request_handler(city)["location"]["localtime"]
+    last_updated = request_handler(city)["current"]["last_updated"]
     if request_handler(city)["current"]["is_day"] == 1:
         day_night = "Day"
     else:
         day_night = "Night"
-    return {"place": place, "weather": condition, "temp_c": temp_c, "temp_f": temp_f,
-            "local_time": local_time, "day_night": day_night}
+    return {"place": place, "region": region, "country": country, "weather": condition, "temp_c": temp_c,
+            "temp_f": temp_f, "local_time": local_time, "day_night": day_night, "last_updated": last_updated}
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from functions import weather
 
-
 if __name__ == '__main__':
     try:
         print("\nWelcome to Weather App. Created by Jayaditya Dev in Python.")
         city = input("Enter the name of the City: ")
-
-        print(f"\nPlace: {weather(city)['place']}")
-        print(f"Weather: {weather(city)['weather']}")
-        print(f"Temperature: {weather(city)['temp_c']}°C / {weather(city)['temp_f']}°F")
-        print(f"Local Time: {weather(city)['local_time']}")
-        print(f"Day/Night: {weather(city)['day_night']}")
-
+        weather_dic = weather(city)
+        print(
+                f"\nPlace: {weather_dic['place']}, {weather_dic['region']}, {weather_dic['country']}\n"
+                f"Weather: {weather_dic['weather']}\n"
+                f"Temperature: {weather_dic['temp_c']}°C / {weather_dic['temp_f']}°F\n"
+                f"Local Time: {weather_dic['local_time']}\n"
+                f"Day/Night: {weather_dic['day_night']}\n"
+                f"\n[Last updated on {weather_dic['last_updated']}]"
+              )
         print("\nThank You!")
     except KeyError:
         print("\nPlease enter a valid city name.")
